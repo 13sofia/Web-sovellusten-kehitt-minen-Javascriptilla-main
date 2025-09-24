85b4363e3a43568fd787976f6d0a420f37ad9766
13sofia <sofia.sallinen@student.laurea.fi>
2025-09-24 14:16:33 +0300

    2

---
+++ WS02_JS_basics2/javakoodit.js
@@ -0,0 +1,33 @@
+//ensimmäinen
+
+document.write('<b><em>"If I had nine hours to chop down a tree, I\'d spend the first six sharpening my ax."</em></b><p>— Abraham Lincoln</p>');
+
+//toinen
+
+for(let i=0; i<50; i++){
+    document.write("Tämä on toistuva teksti.")
+}
+
+//kolme
+
+//Tämä koodi tarkistaa, käyttääkö käyttäjä Mozillan selainta ja ohjaa sen mukaisesti.
+
+
+// Tarkistetaan, sisältääkö selainversion merkkijono "Mozilla"-sanan
+
+/*
+if (navigator.userAgent.includes("Mozilla")) {
+  alert("You are using Firefox, redirecting you...");
+  window.location.href = "http://www.mozilla.org";
+} else {
+  window.location.href = "http://www.google.com";
+}
+*/
+
+//viisi
+
+// kuvan URL-osoite
+const imageUrl = "http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg";
+
+// Kirjoitetaan kuvan HTML-tagi suoraan sivulle
+document.write('<img src="' + imageUrl + '" alt="Kuva Flickr-kuvapalvelusta">');
